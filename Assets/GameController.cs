2f62f0a8049910bd5e7f95fc918205f4754b0b5e
Alexander Runov <st087438@student.spbu.ru>
2021-04-17 22:35:54 +0300

    Доробот Очка -теперь сложность монстров и количество наградных монет за их убийство зависит от этапа -добавлены БОССЫ каждый пятый этап

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -8,22 +8,36 @@ public class GameController : MonoBehaviour
     public double money;
     public double dph;
     public double health;
-    public double healthMax;
+    public double healthMax
+    {
+        get
+        {
+            return 10 * System.Math.Pow(2, stage - 1) * isBoss;
+        }
+    }
+    public double reward;
+
+    public float timer;
+
     public int stage;
     public int stageMax;
     public int kills;
     public int killsMax;
+    public int isBoss;
+    public int timerMax;
 
     public Text moneyText;
     public Text dPHText;
     public Text healthText;
     public Text stageText;
     public Text killsText;
+    public Text timerText;
 
     public GameObject back;
     public GameObject forward;
 
     public Image healthBar;
+    public Image timerBar;
 
     public void Start()
     {
@@ -31,8 +45,9 @@ public class GameController : MonoBehaviour
         stage = 1;
         stageMax = 1;
         killsMax = 10;
-        healthMax = 10;
-        health = healthMax;
+        health = 10;
+        isBoss = 1;
+        timerMax = 30;
     }
 
     public void Update()
@@ -52,6 +67,36 @@ public class GameController : MonoBehaviour
         if (stage != stageMax) forward.gameObject.SetActive(true);
         else
             forward.gameObject.SetActive(false);
+
+        IsBossChecker();
+    }
+
+    public void IsBossChecker()
+    {
+
+        if (stage % 5 == 0)
+        {
+            isBoss = 10;
+            stageText.text = "BOSS!";
+            killsMax = 1;
+            timer = timerMax;
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                stage -= 1;
+                health = healthMax;
+            }
+            timerText.text = timer + "/" + timerMax;
+            timerBar.gameObject.SetActive(true);
+            timerBar.fillAmount = timer / timerMax;
+        }
+        else
+        {
+            isBoss = 1;
+            stageText.text = "Stage " + stage;
+            timerText.text = "";
+            timerBar.gameObject.SetActive(false);
+        }
     }
 
     public void Hit()
@@ -59,8 +104,7 @@ public class GameController : MonoBehaviour
         health -= dph;
         if (health <= 0)
         {
-            money += 1;
-            health = healthMax;
+            money += System.Math.Ceiling(healthMax / 14);
             if (stage == stageMax)
             {
                 kills += 1;
@@ -71,15 +115,18 @@ public class GameController : MonoBehaviour
                     stageMax += 1;
                 }
             }
+            IsBossChecker();
+            health = healthMax;
+            killsMax = 10;
         }
     }
 
-    public void Back()
-    {
-        if (stage > 1) stage -= 1;
-    }
-    public void Forward()
-    {
-        if (stage != stageMax) stage += 1;
-    }
+     public void Back()
+     {
+         if (stage > 1) stage -= 1;
+     }
+     public void Forward()
+     {
+         if (stage != stageMax) stage += 1;
+     }
 }
