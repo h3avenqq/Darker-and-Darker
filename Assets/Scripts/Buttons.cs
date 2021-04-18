064c3907e4d7e71d16c60fa4f3f3b9611035f776
Heaven <shmarev082@gmail.com>
2021-04-18 14:55:20 +0300

    добавил кнопки ads и settings и анимацию для них + подготовил скрипт перехода по нажатию на них

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public string action;
+    public Sprite layer_on, layer_off;
+
+    private void OnMouseDown() {
+        GetComponent <SpriteRenderer>().sprite = layer_on;
+    }
+
+    private void OnMouseUp() {
+        GetComponent <SpriteRenderer>().sprite = layer_off;
+    }
+    private void OnMouseUpAsButton()
+    {
+        switch(action){
+            case "Play":
+            Application.LoadLevel("Play");
+            break;
+            case "Ads":
+
+            break;
+            case "Settings":
+
+            break;
+        }
+    }
+}
